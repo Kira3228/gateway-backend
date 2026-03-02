152939dba790036f29d08e540821129017a321e9
Kira <hanzomein228@gmail.com>
2026-03-02 14:22:18 +0300

    отрефакторил код

--- src/Message/message.service.ts
+++ src/Message/message.service.ts
@@ -1,5 +1,5 @@
 import { inject, injectable, InjectionToken } from "tsyringe";
-import { Repository, } from "typeorm";
+import { Repository, SelectQueryBuilder, } from "typeorm";
 import { Message } from "../Entities";
 import { MessageRequestQuery } from "./types";
 import { MessageConfigService, MessageConfigServiceToken } from "./message-config.service";
@@ -23,6 +23,7 @@ export class MessageService {
   }> {
     try {
       const fields = this.configService.getHeaders(query.presetName)
+
       const fieldsForQb = fields.map((field) => `msg.${field.value}`)
 
       const qb = this.messageRepo
@@ -30,6 +31,17 @@ export class MessageService {
         .select(fieldsForQb)
         .skip(query.limit * (query.page - 1))
         .take(query.limit)
+
+      this.applyMessageParams(qb, {
+        categories: query.categories,
+        metadata: query.metadata,
+        createDateRange: query.createDateRange,
+        updateDateRange: query.updateDateRange,
+        statuses: query.statuses,
+        securityLabels: query.securityLabels,
+        priority: query.priority,
+        messageTypes: query.messageTypes
+      })
 
       if (query.categories) {
         qb.andWhere(`msg.messageCategory IN (:...categories)`, { categories: arrayParser(query.categories) })
@@ -116,6 +128,7 @@ export class MessageService {
           messageTypes: arrayParser(query.messageTypes)
         })
       }
+
       const [messages, messageCount] = await qb.getManyAndCount()
 
       if (!messages) {
@@ -133,4 +146,123 @@ export class MessageService {
       console.error(error);
     }
   }
+
+  async getMessageStream(presetName: string, invisibleFieldsIsAvailable: "true" | "false") {
+    const queryRunner = this.messageRepo.manager.connection.createQueryRunner();
+
+    const fields = await this.configService.getHeaders(presetName);
+    const activeFields = invisibleFieldsIsAvailable === "false"
+      ? fields.filter(f => f.isVisible)
+      : fields;
+
+    const csvHeaders = activeFields.map(f => f.value);
+    const fieldsWithAliases = activeFields.map(f => `msg.${f.value} AS "${f.value}"`);
+
+    const dbStream = await queryRunner.manager
+      .createQueryBuilder(Message, 'msg')
+      .select(fieldsWithAliases)
+    // .stream();
+  }
+
+  private async applyMessageParams<T>(qb: SelectQueryBuilder<T>,
+    params: {
+      priority?: [number, number]
+      metadata?: boolean
+      createDateRange?: [string, string]
+      updateDateRange?: [string, string]
+      categories?: string
+      messageTypes?: string
+      statuses?: string
+      securityLabels?: string
+    }) {
+
+    if (params.categories) {
+      qb.andWhere(`msg.messageCategory IN (:...categories)`, { categories: arrayParser(params.categories) })
+    }
+
+    if (params.metadata) {
+      qb.andWhere(`msg.metadataParsed = :metadata`, { metadata: params.metadata })
+    }
+
+    if (params.createDateRange) {
+      const [startDateRaw, endDateRaw] = params.createDateRange
+
+      const startDate = startDateRaw === '' ? null : startDateRaw
+      const endDate = endDateRaw === '' ? null : endDateRaw
+
+      if (startDate !== null || endDate !== null) {
+        qb.andWhere(
+          `(msg.createdAt >= COALESCE(:startDate, \'1970-01-01\') 
+              AND msg.createdAt <= COALESCE(:endDate, NOW()))`,
+          {
+            startDate,
+            endDate
+          }
+        )
+      }
+    }
+
+    if (params.updateDateRange) {
+      const [startDateRaw, endDateRaw] = params.updateDateRange
+
+      const startDate = startDateRaw === '' ? null : startDateRaw
+      const endDate = endDateRaw === '' ? null : endDateRaw
+
+      if (startDate !== null || endDate !== null) {
+        qb.andWhere(
+          `(msg.updatedAt >= COALESCE(:startDate, \'1970-01-01\') 
+              AND msg.updatedAt <= COALESCE(:endDate, NOW()))`,
+          {
+            startDate,
+            endDate
+          }
+        )
+      }
+    }
+
+    if (params.statuses) {
+      qb.andWhere(`msg.status IN (:...statuses)`, {
+        statuses: arrayParser(params.statuses)
+      })
+    }
+
+    if (params.securityLabels) {
+      qb.andWhere(`msg.securityLabel IN (:...securityLabels)`, {
+        securityLabels: arrayParser(params.securityLabels)
+      })
+    }
+
+    if (params.priority) {
+      const [minPriorityRaw, maxPriorityRaw] = params.priority
+
+      const minPriority = Number(minPriorityRaw)
+      const maxPriority = Number(maxPriorityRaw)
+
+      if (!isNaN(minPriority) || !isNaN(maxPriority)) {
+        const conditions = []
+        const params: any = {}
+
+        if (!isNaN(minPriority)) {
+          conditions.push(`msg.priority >= :minPriority`)
+          params.minPriority = minPriority
+        }
+
+        if (!isNaN(maxPriority)) {
+          conditions.push(`msg.priority <= :maxPriority`)
+          params.maxPriority = maxPriority
+        }
+
+        qb.andWhere(`(${conditions.join(' AND ')})`, params)
+      }
+    }
+
+    if (params.messageTypes) {
+      qb.andWhere(`msg.messageType IN (:...messageTypes)`, {
+        messageTypes: arrayParser(params.messageTypes)
+      })
+    }
+
+    return qb
+  }
+
 }
